81d188007c0ed7db4a7cdbd641eaa64371fdce31
Vaishnavi Gandhi <vaishnavii8319@gmail.com>
2025-11-04 10:49:26 +0530

    done

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,3 +1,17 @@
+// import mongoose from "mongoose";
+
+// const connectDB = async (): Promise<void> => {
+//   try {
+//     const uri = process.env.MONGO_URI!;
+//     await mongoose.connect(uri);
+//     console.log("✅ MongoDB connected");
+//   } catch (error) {
+//     console.error("❌ MongoDB connection error:", error);
+//     throw error;
+//   }
+// };
+
+// export default connectDB;
 import mongoose from "mongoose";
 
 const connectDB = async (): Promise<void> => {
